a9d039429f76be2f9eba1e749f3374129ca404fb
zealouslz <zealouslz@163.com>
2022-10-20 22:44:05 +0800

    登录界面

---
+++ src/components/login/login.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="login">
+      <div class="loginInfo">
+        <img src="../../image/icon.png" class="imgIcon"/>
+        <span class="nameSpan">亲清廉盟在线平台</span>
+        <a-tabs default-active-key="1" @change="callback">
+      <a-tab-pane key="1" tab="账户密码登录">
+        <a-form-model ref="loginForm" :model="loginForm" :rules="rules" >
+    <a-form-model-item   prop="username">
+      <a-input v-model="loginForm.username" placeholder="账户" />
+    </a-form-model-item>
+    <a-form-model-item  prop="username">
+      <a-input v-model="loginForm.password" type="password" placeholder="密码" />
+    </a-form-model-item>
+    <a-form-model-item >
+      <a-checkbox @change="onChange" style="float:left">
+    自动登陆
+  </a-checkbox>
+    </a-form-model-item>
+    <a-form-model-item >
+      <a-button type="primary" style="width:100%" @click="submitForm('ruleForm')">
+        登录
+      </a-button>
+    </a-form-model-item>
+  </a-form-model>
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="">
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="">
+      </a-tab-pane>
+    </a-tabs>
+    <div class="foot">
+      <span>帮助
+
+隐私
+
+条款</span>
+    </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      loginForm: {
+        username: '',
+        password: ''
+
+      }
+
+    }
+  },
+  methods: {
+    callback (key) {
+      console.log(key)
+    },
+    onChange (e) {
+      console.log(e)
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+@import url("./login.css");
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/login/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: '/login',
+      name: 'Login',
       component: HelloWorld
     }
   ]
