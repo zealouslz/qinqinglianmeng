4b32d31da696841c791ceec9c47d4c595a5990df
zealouslz <zealouslz@163.com>
2022-10-22 10:24:03 +0800

    登录页面

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,5 @@ export default {
   width: 100%;
   height: 100%;
 }
+
 </style>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -2,36 +2,35 @@
     <div class="login">
     <div>
         <img src="../../image/icon.svg" class="imgIcon" />
-        <span class="nameSpan">亲清廉盟在线平台</span>
+        <div class="nameSpan">亲清廉盟在线平台</div>
         <div class="loginInfo">
             <span class="loginTab">账号密码登录</span>
             <div class="underline"></div>
             <div class="loginForm">
                 <a-form-model ref="loginForm" :model="loginForm" :rules="rules">
                     <a-form-model-item prop="username">
-                        <a-input v-model="loginForm.username"  placeholder="账户" />
+                        <a-input v-model="loginForm.username"  placeholder="账户" ><img slot="prefix" src="../../image/userIcon.svg" /></a-input>
                     </a-form-model-item>
                     <a-form-model-item prop="username">
-                        <a-input v-model="loginForm.password" type="password" placeholder="密码" />
+                        <a-input v-model="loginForm.password" type="password" placeholder="密码" ><img slot="prefix" src="../../image/passwordIcon.svg" /></a-input>
                     </a-form-model-item>
                     <a-form-model-item>
-                        <a-checkbox @change="onChange" class="checkbox">
+                        <a-checkbox @change="onChange">
                             自动登陆
                         </a-checkbox>
                     </a-form-model-item>
                     <a-form-model-item>
-                        <a-button type="primary" style="width:100%" @click="submitForm('ruleForm')">
+                        <a-button type="primary" style="width:100%;background: #00CDFF;border-radius: 2px;" @click="submitForm('ruleForm')">
                             登录
                         </a-button>
                     </a-form-model-item>
                 </a-form-model>
                 </div>
             <div class="foot">
-                <span>帮助
-
-                    隐私
-
-                    条款</span>
+                <span>帮助</span>
+                <span style="margin-left:60px">隐私</span>
+                <span style="margin-left:60px">条款</span>
+                <div class="copyright">copyright ©️2017 蚂蚁金服体验技术部出品</div>
             </div>
         </div>
     </div>
